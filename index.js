4d7eb1e08ceb15ff77cde6ba0628b4119739e27a
Arti jain <63183434+aj921132@users.noreply.github.com>
2023-09-28 20:46:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World! from ete demo ci-cd')
+  res.send('Hello World! from aws ete demo ci-cd')
 })
 
 app.get('/status', (req, res) => {
